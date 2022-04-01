5341f34291be3b60d912577c1c157c820841a91c
Starozytny <starozytny@hotmail.fr>
2022-04-01 12:41:49 +0200

    form contact anchor

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Inform;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -53,6 +63,34 @@ class AppController extends AbstractController
      */
     public function contact(): Response
     {
-        return $this->render('app/pages/contact/index.html.twig');
+//        return $this->render('app/pages/contact/index.html.twig');
+        return $this->redirectToRoute('app_homepage', ['_fragment' => 'restons-en-contact']);
+    }
+
+    /**
+     * @Route("/rester-informer", name="app_stay_touch")
+     */
+    public function inform(Request $request)
+    {
+        if($request->isMethod("POST")){
+            $em = $this->doctrine->getManager();
+
+            $email = $request->get("email");
+            $critere = $request->get("critere");
+
+            $existe = $em->getRepository(Inform::class)->findOneBy(['email' => $email]);
+            if($critere == "critere" && !$existe){
+                $obj = (new Inform())
+                    ->setEmail($email)
+                ;
+
+                $em->persist($obj);
+                $em->flush();
+
+                $this->addFlash("success", "Ta demande a été prise en compte ! A bientôt !");
+            }
+        }
+
+        return $this->redirectToRoute('app_homepage');
     }
 }
---
+++ src/Entity/Inform.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InformRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InformRepository::class)
+ */
+class Inform extends DataEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = $this->initNewDate();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
