8e7a3a47ce7d91ff49da38c993ebc236d8868bc0
Starozytny <starozytny@hotmail.fr>
2022-09-04 17:40:16 +0200

    split contact

--- assets/app/js/pages/blog.js
+++ assets/app/js/pages/blog.js
@@ -5,5 +5,12 @@ import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { ContactForm } from "./components/Contact/ContactForm";
 
 Routing.setRoutingData(routes);
+
+let el = document.getElementById("contact");
+if(el){
+    console.log(el)
+    render(<ContactForm />, el)
+}
