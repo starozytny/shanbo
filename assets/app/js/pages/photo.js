245856849683d1251c487ed2fc53d3a45986608e
Starozytny <starozytny@hotmail.fr>
2022-09-06 21:16:19 +0200

    add masonry

--- assets/app/js/pages/photo.js
+++ assets/app/js/pages/photo.js
@@ -1,1 +1,11 @@
 import '../../css/pages/photo.scss';
+
+import Masonry from "masonry-layout/dist/masonry.pkgd.min";
+
+let elem = document.querySelector('.grid');
+let msnry = new Masonry( elem, {
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+    gutter: 24,
+});
