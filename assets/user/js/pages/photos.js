4e66291cda01e6de01101ae45cb094fbf2a93b2c
Shanbora <chanbora@logilink.fr>
2022-09-29 15:53:22 +0200

    mansory

--- assets/user/js/pages/photos.js
+++ assets/user/js/pages/photos.js
@@ -3,12 +3,14 @@ import "../../css/pages/photos.scss";
 const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
-import React from "react";
-import Modal from "@commonComponents/functions/modal";
+import React   from "react";
+import Modal   from "@commonComponents/functions/modal";
+import Mansory from "@commonComponents/functions/mansory";
 import { render } from "react-dom";
 import { AddPhoto } from "@userPages/components/Photo/AddPhoto";
 
 Modal.managaModal();
+Mansory.loadMansory();
 
 Routing.setRoutingData(routes);
 
