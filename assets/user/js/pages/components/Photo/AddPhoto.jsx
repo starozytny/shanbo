2c18303d3ee8ddbd0b020b304c4caa8b27341ea0
Shanbora <chanbora@logilink.fr>
2022-09-29 15:01:26 +0200

    init modal with photo

---
+++ assets/user/js/pages/components/Photo/AddPhoto.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+import Helper from "@commonComponents/functions/helper";
+
+import { Input } from "@dashboardComponents/Tools/Fields";
+
+export class AddPhoto extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            photos: [],
+            errors: []
+        }
+
+        this.handleChangeFile = this.handleChangeFile.bind(this);
+    }
+
+    handleChangeFile = (e) => {
+        const { photos } = this.state;
+
+        let files = e.target.files;
+        let self = this;
+
+        let rank = 1;
+        photos.forEach(p => {
+            if(p.isTrash !== true){
+                rank++;
+            }
+        })
+        if(files){
+            Array.prototype.forEach.call(files, (file) => {
+                if(rank <= 20){
+                    if(file.size > 30048576){
+                        toastr.error("Le fichier est trop gros.")
+                    }else{
+                        if (/\.(jpe?g|png|gif)$/i.test(file.name)){
+                            Helper.getBase64(file, self, rank);
+                            rank++;
+                        }
+                    }
+                }else{
+                    toastr.error("Vous avez atteint la limite max de photos.")
+                }
+            })
+        }
+    }
+
+    render () {
+        const { errors, photos } = this.state;
+
+        return <div>
+            <Input type="file" identifiant="photos" isMultiple={true} valeur={photos}
+                   acceptFiles={".jpg, .jpeg, .png"}
+                   errors={errors} onChange={this.handleChangeFile}>
+                <span>Photos</span>
+            </Input>
+        </div>
+    }
+}
--- assets/user/js/pages/photos.js
+++ assets/user/js/pages/photos.js
@@ -1,6 +1,18 @@
 import "../../css/pages/photos.scss";
 
+const routes = require('@publicFolder/js/fos_js_routes.json');
+import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
+
+import React from "react";
 import Modal from "@commonComponents/functions/modal";
+import { render } from "react-dom";
+import { AddPhoto } from "@userPages/components/Photo/AddPhoto";
 
 Modal.managaModal();
 
+Routing.setRoutingData(routes);
+
+let el = document.getElementById("drop-add-photos");
+if(el){
+    render(<AddPhoto type="create" />, el)
+}
