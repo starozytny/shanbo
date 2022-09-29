d20492d9c0e9aff7c09c1e9f75550c0ab1da52a1
Shanbora <chanbora@logilink.fr>
2022-09-29 14:42:19 +0200

    add modal logic

--- assets/user/js/pages/photos.js
+++ assets/user/js/pages/photos.js
@@ -1,1 +1,19 @@
 import "../../css/pages/photos.scss";
+
+modal();
+
+function modal () {
+    let btnsModal = document.querySelectorAll('.modal-btn');
+    btnsModal.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            let modal = document.querySelector(btn.dataset.modal);
+            if(modal){
+                if(modal.classList.contains('active')){
+                    modal.classList.remove('active');
+                }else{
+                    modal.classList.add('active');
+                }
+            }
+        })
+    })
+}
