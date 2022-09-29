3994b186fbe8a26b36ff59eebbe64bd2c7132f74
Shanbora <chanbora@logilink.fr>
2022-09-29 14:43:48 +0200

    facto modal

--- assets/user/js/pages/albums.js
+++ assets/user/js/pages/albums.js
@@ -4,9 +4,12 @@ const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
 import React from "react";
+import Modal from "@commonComponents/functions/modal";
 import { render } from "react-dom";
 import { AlbumFormulaire } from "@userPages/components/Album/AlbumForm";
 import { AlbumRead } from "@userPages/components/Album/AlbumRead";
+
+Modal.managaModal();
 
 Routing.setRoutingData(routes);
 
--- assets/user/js/pages/photos.js
+++ assets/user/js/pages/photos.js
@@ -1,19 +1,6 @@
 import "../../css/pages/photos.scss";
 
-modal();
+import Modal from "@commonComponents/functions/modal";
 
-function modal () {
-    let btnsModal = document.querySelectorAll('.modal-btn');
-    btnsModal.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            let modal = document.querySelector(btn.dataset.modal);
-            if(modal){
-                if(modal.classList.contains('active')){
-                    modal.classList.remove('active');
-                }else{
-                    modal.classList.add('active');
-                }
-            }
-        })
-    })
-}
+Modal.managaModal();
+
